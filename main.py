3c3f9aec618f0e4f18e3752267c895b89076ba94
jmuriki <jmuriki@mail.ru>
2023-09-25 14:11:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import requests
 
 
-def show_weather(locations, config):
+def get_weather(locations, config):
 	url_template = "https://wttr.in/{}"
+	weather_payload = []
 	for location in locations:
 		url = url_template.format(location)
 		response = requests.get(url, params=config)
 		response.raise_for_status()
-		print(response.text)
+		weather_payload.append(response.text)
+	return weather_payload
 
 
 def main():
@@ -23,8 +25,9 @@ def main():
 		"M": "",
 		"lang": "ru"
 	}
-	show_weather(locations, config)
-
+	weather_payload = get_weather(locations, config)
+	for location_weather in weather_payload:
+		print(location_weather)
 
 
 if __name__ == "__main__":
